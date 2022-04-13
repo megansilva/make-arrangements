3082ea0f57a81fb1b5492b021074bb1ade522b4e
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-04-13 17:17:33 -0400

    saving for today

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-// let daySchedule = {
-//     "8AM": "",
-//     "9AM": "",
-//     "10AM": "",
-//     "11AM": "",
-//     "12PM": "",
-//     "1PM": "",
-//     "2PM": "",
-//     "3PM": "",
-//     "4PM": "",
-//     "5PM": ""
-// };
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-// $(document).ready(function() {
-//     if(!localStorage.getItem('daySchedule')) {
-//         updateCalendarTasks(daySchedule);
-//     } else {
-//         updateCalendarTasks(JSON.parse(localStorage.getItem('daySchedule')));
-//     }
-// })
+    $(".saveBtn").on("click", function() {
+        console.log(this);
+        var text = $(this).sibiling(".description").val();
+        var time = $(this).parent().attr("id");
 
-// $('#date-today h6').text(moment().format('dddd'+", " + moment().format('MMMM Do YYYY, h:mm:ss a')));
+        localStorage.setItem(time, text);
+    })
 
-// let counter = 1;
-// for(const property in daySchedule) {
-//     let textEntry = "#text-entry" + counter;
-//     $(textEntry).text(daySchedule[property]);
-//     let timeId= "#time" + counter;
-//     let presentHour = moment().hour();
-//     let timeString = $(timeId).text();
+    $("#time1 .description").val(localStorage.getItem("time1"));
+    $("#time2 .description").val(localStorage.getItem("time2"));
+    $("#time3 .description").val(localStorage.getItem("time3"));
+    $("#time4 .description").val(localStorage.getItem("time4"));
+    $("#time5 .description").val(localStorage.getItem("time5"));
+    $("#time6 .description").val(localStorage.getItem("time6"));
+    $("#time7 .description").val(localStorage.getItem("time7"));
+    $("#time8 .description").val(localStorage.getItem("time8"));
+    $("#time9 .description").val(localStorage.getItem("time9"));
+    $("#time10 .description").val(localStorage.getItem("time10"));
 
-// }
+    function hourTrack() {
+        var currentHour = moment().hout();
+    }
+
+        $(".time-block").each(funtion())
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(blockHour,currentHour)
+
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            } else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            } else {
+                $(this).removeClass("past");
+                $(this).removelass("present");
+                $(this).addClass("future");
+            }
+        })
+
+
+
